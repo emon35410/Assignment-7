395cb159ee64932292b5039e3bd24d16011f1708
emon35410 <mhhsan341@gmail.com>
2025-09-26 10:43:04 +0600

    background Image set

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import bgImg from '../../assets/vector1.png'
-import bgImg2 from '../../assets/vector2.png'
+import bgImg2 from '../../assets/bgimage2.png'
 
 const Banner = () => {
     return (
         <div className='flex flex-col  md:flex-row justify-center-safe gap-6 mt-20'>
             <div className=' w-full h-[150px]  flex justify-center items-center  md:w-[508px] md:h-[250px] rounded-lg '
                 style={{
-                    backgroundImage: `linear-gradient(125.07deg,#632ee3,#9f62f2), url('${bgImg}')`,
-                    backgroundSize: 'cover',
+                    backgroundImage: `url(${bgImg}), url(${bgImg2}), linear-gradient(125.07deg,#632ee3,#9f62f2)`,
+                    backgroundSize: '150px 100%, 150px 100%, cover',
                     backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center'
+                    backgroundPosition: 'left top, right top, center'
                 }}
             >
                 <div className='flex flex-col justify-center items-center gap-1 text-white'>
@@ -19,11 +19,13 @@ const Banner = () => {
                 </div>
             </div>
             <div className=' w-full h-[150px] flex gap-1 justify-center items-center   md:w-[508px] md:h-[250px] rounded-lg '
-             style={{
-                    backgroundImage: `linear-gradient(90.00deg,#54cf68,#00827a), url('${bgImg2}')`,
-                    backgroundSize: 'cover',
+                style={{
+
+                    backgroundImage: `url(${bgImg}), url(${bgImg2}), linear-gradient(90.00deg,#54cf68,#00827a)`,
+                    backgroundSize: '150px 100%, 150px 100%, cover',
                     backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center'
+                    backgroundPosition: 'left top, right top, center'
+
                 }}
             >
                 <div className='flex flex-col justify-center items-center text-white'>
---
+++ src/component/CustomerCard/CustomerCard.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import greenCircleImg from '../../assets/circle-green.png'
+import yellowCircleImg from '../../assets/circle-yellow.png'
+import calenderImg from '../../assets/calendar-line.png'
+
+const CustomerCard = ({ticket}) => {
+    return (
+        <div className='mt-2 p-3 md:w-[430px] md:h-[160px]  bg-white shadow-sm rounded-2xl'>
+                        <div className='flex justify-between items-center mb-2 roun'>
+                            <h1 className='font-medium'>{ticket.title}</h1>
+                            <div className={`font-medium flex items-center justify-center p-1 rounded-2xl gap-1 bg-green-300`}>
+
+                                <img src={greenCircleImg} alt="" />
+                                <span>Open</span>
+                            </div>
+                        </div>
+                        <p className='mb-1 text-[#637E96]'>{ticket.description}</p>
+                        <div className='flex justify-between items-center'>
+                            <div className='flex justify-center gap-2 items-center'>
+                                <h1 className='text-[#637E96]'>#{ticket.id}</h1>
+                                <h1 className={`font-medium ${ticket.priority==="High Priority"?"text-red-500":ticket.priority==="Low Priority"?"text-green-500":"text-yellow-500"}`}>{ticket.priority}</h1>
+                            </div>
+                            <div className='text-[#637E96] flex justify-center gap-3 items-center'>
+                                <h1 >{ticket.customer}</h1>
+                                <div className='flex justify-center gap-3 items-center'>
+                                    <img src={calenderImg} alt="" />
+                                    <h1>{ticket.createdAt}</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    );
+};
+
+export default CustomerCard;
